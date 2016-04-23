1ee24250cf56bc0552171ccf8cc31af7a36b1705
System User <a@a.com>
2016-04-23 10:08:25 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Dojo 4 NodeJS
+
+# Desarrollo de aplicaciones empresariales
+
+Una mini api para una aplicación de saldos usando NodeJS, básicos de GET y POST y una conexión a una base de datos postgres
---
+++ server.js
@@ -0,0 +1,33 @@
+console.log("Inició el mini api");
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var pg = require("pg");
+var app = express();
+
+var db = 'postgres://njjjgqmr:8lsQTTn-pTexFkPW2y3Hyy8G_5fT5E6u@pellefant.db.elephantsql.com:5432/njjjgqmr';
+var client = new pg.Client(db);
+client.connect(function(err){
+	if(err){
+		console.log("Error en la conexión a la db");
+		return;
+	}
+	console.log("Se ha realizado la conexión con la base de datos");
+	client.end();
+});
+var PORT = 3777;
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(PORT,function(){
+	console.log("Escuchando en el puerto: "+PORT);
+});
+
+app.get("/",function(req,res){
+	res.send("Bienvenido al mini api de saldos");
+});	
+
+app.post("/postPrueba",function(req,res){
+	console.log(req.body);
+	res.status(201).send("Okay");
+});
