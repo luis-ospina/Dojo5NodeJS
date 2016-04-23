d88fd76a91cc5a514a06a6b3461802a399ba3434
System User <a@a.com>
2016-04-23 10:09:56 -0500

    Añadiendo el método para insertar a la base de datos

--- server.js
+++ server.js
@@ -31,3 +31,32 @@ app.post("/postPrueba",function(req,res){
 	console.log(req.body);
 	res.status(201).send("Okay");
 });
+
+app.post('/api/insertar', function(req, res){
+    var cedula = req.body.cedula;
+    var nombre = req.body.nombre;
+    var dinero = req.body.dinero;
+    
+    var queryInsertar = 'INSERT INTO saldo VALUES('
+                + cedula + ', '
+                + '\'' + nombre + '\', '
+                + dinero + ');'
+    console.log(queryInsertar);
+    
+    pg.connect(URL, function(err, client, done){
+        if (err){
+            res.send('Error :(');
+            return console.log('Error de conexión');
+        }
+        client.query(queryInsertar, function(err, result){
+            if(err){
+                res.send('Error :(');
+                client.end();
+                return console.log('Error en el query');
+            }
+            console.log('Se insertó');
+            res.send('Okay');
+            client.end();
+        });
+    });
+});
